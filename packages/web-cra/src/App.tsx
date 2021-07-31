518a6ac1fd0cf2ad8d0f4daa04a1edb520221cc5
Taeho Kim <taeho@taeho.io>
2021-07-31 16:54:49 +0900

    Add web-cra package

---
+++ packages/web-cra/src/App.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Button, StyleSheet, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+type RootStackParamList = {
+  Home: undefined;
+  Profile: undefined;
+};
+
+const HomeScreen = ({
+  navigation,
+}: {
+  navigation: StackNavigationProp<RootStackParamList, 'Home'>;
+}) => {
+  return (
+    <View style={styles.screen}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  );
+};
+
+const ProfileScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+};
+
+const HomeStack = createStackNavigator();
+
+const HomeStackScreen = () => {
+  return (
+    <HomeStack.Navigator initialRouteName="Home">
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Profile" component={ProfileScreen} />
+    </HomeStack.Navigator>
+  );
+};
+
+const linkConfig = {
+  screens: {
+    HomeStack: {
+      screens: {
+        Home: 'home',
+        Profile: 'user',
+      },
+    },
+  },
+};
+
+const linking = {
+  prefixes: ['http://localhost:3000'],
+  config: linkConfig,
+};
+
+const Tab = createBottomTabNavigator();
+
+export const App = () => {
+  return (
+    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
+      <HomeStackScreen />
+    </NavigationContainer>
+  );
+};
+
+export default App;
