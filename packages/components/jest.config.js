792b96f8732e7f159af4834940ad8f228c165ff2
Taeho Kim <taeho@taeho.io>
2021-08-03 21:41:28 +0900

    Add componeent tests

--- packages/components/jest.config.js
+++ packages/components/jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: ['node_modules/(?!(react-native|@react-native)/)'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/src/__mocks__/fileMock.js',
+  },
 };
