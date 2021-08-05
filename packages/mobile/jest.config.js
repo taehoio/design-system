6e3f7ffc203391afb0878b1f6ba5f11431231e40
Taeho Kim <taeho@taeho.io>
2021-08-05 16:51:08 +0900

    Add magnus ui (#24)

--- packages/mobile/jest.config.js
+++ packages/mobile/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-vector-icons|react-native-gesture-handler|react-native-iphone-x-helper)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-vector-icons|react-native-gesture-handler|react-native-iphone-x-helper|react-native-animatable|react-native-magnus|react-native-modal)/)',
   ],
   setupFiles: ['<rootDir>/jest.setup.js'],
   timers: 'fake',
